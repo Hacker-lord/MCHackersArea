c7f2ecdca591b61ca60bd5bec57927374afbe7ed
Hacker-lord <64102828+Hacker-lord@users.noreply.github.com>
2020-04-22 15:47:58 -0500

    Update system.js

--- docs/system.js
+++ docs/system.js
@@ -3,5 +3,11 @@ function goto(x) {
 }
 function encrypted5g(x) {
   //Safety feature
-  
+  var splitString = x.split("");
+  var reverseArray = splitString.reverse();
+  var joinArray = reverseArray.join("");
+  return joinArray;
+}
+function server(x) {
+   goto(encrypted5g(x));
 }
